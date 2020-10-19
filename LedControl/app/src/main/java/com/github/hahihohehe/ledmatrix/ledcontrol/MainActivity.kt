8a0c484ca3e432cfe750c8105c0feda058222e89
hahiho <n.a@live.de>
2020-10-19 13:38:53 +0200

    Added android app project files

---
+++ LedControl/app/src/main/java/com/github/hahihohehe/ledmatrix/ledcontrol/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.github.hahihohehe.ledmatrix.ledcontrol
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.github.hahihohehe.ledmatrix.ledcontrol.ui.main.MainFragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.container, MainFragment.newInstance())
+                .commitNow()
+        }
+    }
+}
